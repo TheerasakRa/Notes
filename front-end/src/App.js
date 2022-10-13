455fe6921f3aa9a19901708652c44ea1af6d3f3b
Theerasak Rachphonsaen <theerasak.ra.63@ubu.ac.th>
2022-10-13 23:47:55 +0700

    update app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div class="grid w-screen h-screen bg-[url('https://images3.alphacoders.com/536/53625.jpg')] bg-cover bg-center ">
-        <div class="w-full h-full grid-cols-3 backdrop-blur-lg">
+        <div class="w-full h-full grid-cols-3 backdrop-blur-lg" >
           <Notes />
         </div>
       </div>
